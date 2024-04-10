5c84a4a46fe6f1c5438a1d65f15219cd24624109
Godwin peter .O <me@godwin.dev>
2024-04-10 01:29:09 +0100

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace RandomMessages
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace RandomMessages
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
